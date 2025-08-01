fad50aa6363fe7cb03df2980319cbf0f3ef38d7c
Fatemeh Karimi <145932901+Fatemeh-Karimii@users.noreply.github.com>
2025-08-01 15:38:20 -0400

    Sensitivity analysis 1.py

--- Sensitivity analysis 1.py
+++ Sensitivity analysis 1.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 30 11:07:29 2023
-
-@author: Se7eN.CO
-"""
-
-
 from mip import *
-
 import numpy as np
-
 import random
 
 Year = [0, 1, 2, 3, 4, 5] 
@@ -17,7 +7,7 @@ Product = ["Coal", "Steel", "Transport"]
 
 c = np.array([[0.1, 0.1 , 0.2 , 0.6],[0.5, 0.1 , 0.1 , 0.3],[0.4 , 0.2 , 0.2 , 0.2]])
 d = np.array([[0 , 0.1 , 0.2 , 0.4],[0.7 , 0.1 , 0.1 , 0.2],[0.9 , 0.2 , 0.2 , 0.1]])
-a = random.uniform(0.8, 1.2) #ضریب
+a = random.uniform(0.8, 1.2) 
 e1= np.array([[0,60,60,60,60,60],[0,60,60,60,60,60],[0,30,30,30,30,30]])
 e=a*e1
 p = np.array([[300], [350], [280]])
@@ -50,7 +40,7 @@ m.objective = maximize(xsum(c[i][3] * x[i][t+1] for t in range(6) for i in range
  
 print("a:",a)         
 m.optimize()
-print("مقدار تابع هدف :" ,m.objective_value)
+print("Objective function value:" ,m.objective_value)
 
 
 for i in range(3):
